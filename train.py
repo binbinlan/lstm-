52925b9c56e9263232764b96e9633fad75ea6de5
binbinlan <787250087@qq.com>
2021-08-23 16:09:50 +0800

    2021.8.23 go back home

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from math import sqrt
 from numpy import concatenate
 from matplotlib import pyplot
+import pandas
 from pandas import read_csv
 from pandas import DataFrame
 from pandas import concat
@@ -11,6 +12,31 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.models import load_model
+pandas.set_option('display.max_columns', None)
+pandas.set_option('display.max_rows', None)
+
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+	n_vars = 1 if type(data) is list else data.shape[1]
+	df = DataFrame(data)
+	cols, names = list(), list()
+	# input sequence (t-n, ... t-1)
+	for i in range(n_in, 0, -1):
+		cols.append(df.shift(i))
+		names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+	# forecast sequence (t, t+1, ... t+n)
+	for i in range(0, n_out):
+		cols.append(df.shift(-i))
+		if i == 0:
+			names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+		else:
+			names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+	# put it all together
+	agg = concat(cols, axis=1)
+	agg.columns = names
+	# drop rows with NaN values
+	if dropnan:
+		agg.dropna(inplace=True)
+	return agg
 
 def training(data,N_hours,N_features):
     #load dataset
@@ -23,4 +49,25 @@ def training(data,N_hours,N_features):
     n_hours = N_hours
     n_features = N_features
     print(n_hours,n_features)
+    # frame as supervised learning
+    reframed = series_to_supervised(scaled, n_hours, 1)
+    #reframed = series_to_supervised(values, n_hours, 1)
+    print(reframed.shape)
+    print(reframed.head(5))
+
+    # split into train and test sets
+    values = reframed.values
+    #n_train_hours = 365 * 24
+    n_train_hours = 10
+    train = values[:n_train_hours, :]
+    test = values[n_train_hours:, :]
+    # split into input and outputs
+    n_obs = n_hours * n_features
+    train_X, train_y = train[:, :n_obs], train[:, -1]
+    test_X, test_y = test[:, :n_obs], test[:, -1]
+    print(train_X.shape, len(train_X), train_y.shape)
+    # reshape input to be 3D [samples, timesteps, features]
+    train_X = train_X.reshape((train_X.shape[0], n_hours, n_features))
+    test_X = test_X.reshape((test_X.shape[0], n_hours, n_features))
+    print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
 
